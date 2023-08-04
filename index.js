e10f0b93721c920f02741fcacbb9a0717194a34b
sergius <autumnus.et.folium@gmail.com>
2023-08-04 21:31:35 +0300

    two

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// eslint-disable-next-line no-unused-vars
+/* // eslint-disable-next-line no-unused-vars
 let obj = {
 	a: 33,
 	b: 44,
@@ -14,4 +14,51 @@ let bb = "11";
 console.log(obj[bb]);
 obj[bb] = bb;
 console.log(obj);
-console.log(obj["11"]);
+console.log(obj["11"]); */
+
+/* const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+
+const personalMovieDB = {
+	count: numberOfFilms,
+	movies: {},
+	actors: {},
+	genres: [],
+	privat: false
+};
+
+const a = prompt("Один из последних просмотренных фильмов?", ""),
+	b = prompt("На сколько оцените его?", ""),
+	c = prompt("Один из последних просмотренных фильмов?", ""),
+	d = prompt("На сколько оцените его?", "");
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+//console.log(personalMovieDB.movies[a]);
+console.log(personalMovieDB); */
+
+
+/* //Что выведет в консоль код?
+console.log( NaN || 2 || undefined ); // 2
+
+
+console.log( NaN && 2 && undefined ); //nan
+
+
+console.log( 1 && 2 && 3 ); //3
+
+
+console.log( !1 && 2 || !3 ); //!1/true
+
+
+console.log( 25 || null && !3 ); //25
+
+
+console.log( NaN || null || !3 || undefined || 5); //5
+
+
+console.log( NaN || null && !3 && undefined || 5); //5
+
+
+console.log( 5 === 5 && 3 > 1 || 5); //5===5/true */
+
+(1 > 0) ? console.log("1>0") : console.log("1<=0");
