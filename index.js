a29a470d64e20081402c0eb1a9ee23461311cec9
sergius <autumnus.et.folium@gmail.com>
2023-08-05 12:19:43 +0300

    вводи значения мудила

--- index.js
+++ index.js
@@ -97,3 +97,186 @@ if (hamburger && cola || fries === 3 && nuggets) { //false
 } else {
 	console.log("идите на хуй я вас не звал лжывые ублюдки");
 } */
+/* console.log("abc".repeat(6)+"/n"); // '' */
+
+/* let result = "";
+for (let i = 1; i < 8; i++) {
+	result += "*".repeat(i) + "\n";
+}
+console.log(result.trim()); */
+
+/* function fifthTask() {
+	const arrayOfNumbers = [];
+
+	for (let i = 5; i < 11; i++) {
+		//arrayOfNumbers[i - 5] = i;
+		arrayOfNumbers.push(i);
+	}
+
+	console.log(arrayOfNumbers);
+	return arrayOfNumbers;
+}
+console.log(fifthTask()); */
+
+/* function firstTask() {
+	const arr = [3, 5, 8, 16, 20, 23, 50];
+	const result = [];
+
+	for (let i = 0; i < arr.length; i++) {
+		//result[i] = arr[i];
+		result.push(arr[i]);
+	}
+
+	console.log(result);
+	return result;
+}
+
+firstTask(); */
+
+/* function secondTask() {
+	const data = [5, 10, "Shopping", 20, "Homework"];
+
+	for (let i = 0; i < data.length; i++) {
+		if (typeof(data[i]) === "number") {
+			data[i] = data[i] * 2;
+		} else if (typeof(data[i]) === "string") {
+			data[i] = `${data[i]} - done`;
+		}
+	}
+
+	console.log(data);
+	return data;
+}
+
+secondTask();
+
+function thirdTask() {
+	const data = [5, 10, "Shopping", 20, "Homework"];
+	const result = [];
+
+	for (let i = 1; i <= data.length; i++) {
+		result[i - 1] = data[data.length - i];
+	}
+
+	console.log(result);
+	return result;
+}
+
+thirdTask(); */
+
+/* const lines = 5;
+let result = "";
+ 
+for (let i = 0; i <= lines; i++) {
+	for (let j = 0; j < lines - i; j++) {
+		result += " ";
+	}
+	for (let k = 0; k < 2 * i + 1; k++) {
+		result += "*";
+		console.log(k);
+	}
+	result += "\n";
+}
+ 
+console.log(result); */
+
+/* Задание на урок:
+
+1) Автоматизировать вопросы пользователю про фильмы при помощи цикла
+
+2) Сделать так, чтобы пользователь не мог оставить ответ в виде пустой строки,
+отменить ответ или ввести название фильма длинее, чем 50 символов. Если это происходит - 
+возвращаем пользователя к вопросам опять
+
+3) При помощи условий проверить  personalMovieDB.count, и если он меньше 10 - вывести сообщение
+"Просмотрено довольно мало фильмов", если от 10 до 30 - "Вы классический зритель", а если больше - 
+"Вы киноман". А если не подошло ни к одному варианту - "Произошла ошибка"
+
+4) Потренироваться и переписать цикл еще двумя способами*/
+
+"use strict";
+
+// Код возьмите из предыдущего домашнего задания
+let h = document.querySelector("h1");
+
+const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+
+const personalMovieDB = {
+	count: numberOfFilms,
+	movies: {},
+	actors: {},
+	genres: [],
+	privat: false
+};
+
+/* for(let i = 0; i < 2; i++) {
+	const a = prompt("Один из последних просмотренных фильмов?", ""),
+		b = prompt("На сколько оцените его?", "");
+	if (a != null && a != "" && b != null && b != "" && a.length <= 50 && b.length <= 2) {
+		personalMovieDB.movies[a] = b;
+	} else {
+		console.log("вводи значения мудила");
+		i--;
+	}
+} */
+let i = 0;
+/* do {
+	const a = prompt("Один из последних просмотренных фильмов?", ""),
+		b = prompt("На сколько оцените его?", "");
+	if (a != null && a != "" && b != null && b != "" && a.length <= 50 && b.length <= 2) {
+		personalMovieDB.movies[a] = b;
+	} else {
+		console.log("вводи значения мудила");
+		i--;
+	} 
+	i++;
+} while ( i < 2); */
+
+while (i < 2) {
+	const a = prompt("Один из последних просмотренных фильмов?", ""),
+		b = prompt("На сколько оцените его?", "");
+	if (a != null && a != "" && b != null && b != "" && a.length <= 50 && b.length <= 2) {
+		personalMovieDB.movies[a] = b;
+	} else {
+		console.log("вводи значения мудила");
+		i--;
+	}
+	i++;
+}
+
+/* if(personalMovieDB.count < 10) {
+	//console.log("Просмотрено довольно мало фильмов");
+	h.textContent = "Просмотрено довольно мало фильмов";
+
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30) {
+	//console.log("Просмотрено довольно много фильмов");
+	h.textContent = "Просмотрено довольно много фильмов";
+} else if (personalMovieDB.count >= 30) {
+	//console.log("Вы киноман");
+	h.textContent = "Вы киноман";
+} else {
+	//console.log("Произошла ошибка");
+	h.textContent = "Произошла ошибка";
+} */
+
+switch (true) {
+case personalMovieDB.count < 10:
+	h.textContent = "Просмотрено довольно мало фильмов";
+	break;
+case personalMovieDB.count >= 10:
+	h.textContent = "Просмотрено довольно много фильмов";
+	break;
+
+case personalMovieDB.count >= 30:
+	h.textContent = "Вы киноман";
+	break;
+default:
+	h.textContent = "Произошла ошибка";
+}
+
+//тернарный оператор не подходит для множества проверок
+/* (personalMovieDB.count < 10) ? h.textContent = "Просмотрено довольно мало фильмов" : h.textContent = "Произошла ошибка";
+(personalMovieDB.count >= 10) ? h.textContent = "Просмотрено довольно много фильмов" : h.textContent = "Произошла ошибка";
+(personalMovieDB.count >= 30) ? h.textContent = "Вы киноман" : h.textContent = "Произошла ошибка"; */
+
+console.log(personalMovieDB);
